9b979159cbaa7b702130702dd3fdd944eb0d79c9
Badreddine DLAILA <dlila.badreddine@live.fr>
2019-01-13 21:02:29 +0100

    Add status controller

---
+++ DialogFlow.OH.Proxy/Controllers/StatusController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DialogFlow.OH.Proxy.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StatusController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<string> Get()
+        {
+            return "Proxy service up and running !";
+        }
+
+    }
+}
