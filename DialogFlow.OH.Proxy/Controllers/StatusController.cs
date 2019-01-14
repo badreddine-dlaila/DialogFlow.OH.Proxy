eaf82961673409a6d04d6d4726b10487d1604039
Badreddine DLAILA <dlila.badreddine@live.fr>
2019-01-14 01:29:11 +0100

    Add info route

--- DialogFlow.OH.Proxy/Controllers/StatusController.cs
+++ DialogFlow.OH.Proxy/Controllers/StatusController.cs
@@ -16,5 +16,44 @@ namespace DialogFlow.OH.Proxy.Controllers
             return "Proxy service up and running !";
         }
 
+        [HttpGet]
+        [Route("Info")]
+        public ActionResult<dynamic> Info()
+        {
+            return new
+            {
+                HomeDashboard = "http://176.130.227.69:8080",
+                Webhook = "https://smarthome-proxy.azurewebsites.net/api/webhook/",
+                DialogFlowProjectExtract = "",
+                Rooms = new List<string>
+                {
+                    "Entryway",
+                    "LivingDining",
+                    "Kitchen",
+                    "LaundryRoom",
+                    "Toilet",
+                    "MasterBedroom",
+                    "Bedroom",
+                    "Bathroom",
+                    "Toilet",
+                    "Garage"
+                },
+                SupportedDevices = new List<dynamic>
+                {
+                    new { Device = "Light", Command = "brightness", Room = "LivingDining" },
+                    new { Device = "Light", Command = "brightness", Room = "MasterBedroom" },
+                    new { Device = "Light", Command = "0-100", Room = "Entryway" },
+                    new { Device = "Light", Command = "0-100", Room = "Kitchen" },
+                    new { Device = "Light", Command = "0-100", Room = "LaundryRoom" },
+                    new { Device = "Light", Command = "0-100", Room = "Toilet" },
+                    new { Device = "Light", Command = "0-100", Room = "Bathroom" },
+                    new { Device = "Light", Command = "0-100", Room = "Bedroom" },
+
+                    new { Device = "Heating", Command = "0-100", Room = "LivingDining" },
+                    new { Device = "Heating", Command = "0-1OO", Room = "Bathroom" },
+                }
+            };
+        }
+
     }
 }
